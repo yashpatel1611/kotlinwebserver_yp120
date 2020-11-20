999b254b2608b6342f9e0529290e127d862672a5
yashle1611 <yashpatel1611@gmail.com>
2020-11-20 11:18:48 +0000

    Cleaned up lint issues

--- src/main/kotlin/webserver/WebServer.kt
+++ src/main/kotlin/webserver/WebServer.kt
@@ -2,20 +2,20 @@ package webserver
 
 // write your web framework code here:
 
-//Uses the string function 'substringBefore' to get everything before
-//the first ':' character
+// Uses the string function 'substringBefore' to get everything before
+// the first ':' character
 fun scheme(url: String): String = url.substringBefore(':')
 
-//Uses the spring split function to separate all the parts between '/'s
-//The second index of the set of separations is the host url
+// Uses the spring split function to separate all the parts between '/'s
+// The second index of the set of separations is the host url
 fun host(url: String): String = url.split('/')[2]
 
-//Uses the substringAfter function on the host url to get the substring before the parameters
+// Uses the substringAfter function on the host url to get the substring before the parameters
 fun path(url: String): String = url.substringAfter(host(url)).substringBefore("?")
 
-//Function to return the list of pair parameters in a url
-//The function first gets all the parameters and splits them into each pair
-//The pairs are looped through using a for loop and are added to the mutable list
+// Function to return the list of pair parameters in a url
+// The function first gets all the parameters and splits them into each pair
+// The pairs are looped through using a for loop and are added to the mutable list
 fun queryParams(url: String): List<Pair<String, String>> {
 
     val params = url.substringAfter("?").split('&')
@@ -30,9 +30,11 @@ fun queryParams(url: String): List<Pair<String, String>> {
     return pairList
 }
 
-//List of pair values with the path and its corresponding handler
-var mappings = listOf(Pair("/say-hello", ::helloHandler), Pair("/", ::homePageHandler),
-        Pair("/computing", ::compPageHandler))
+// List of pair values with the path and its corresponding handler
+var mappings = listOf(
+    Pair("/say-hello", ::helloHandler), Pair("/", ::homePageHandler),
+    Pair("/computing", ::compPageHandler)
+)
 
 fun route(request: Request): Response {
     val pagePath = path(request.url)
@@ -46,30 +48,28 @@ fun route(request: Request): Response {
 
 // http handlers for a particular website...
 
-//Handler for main homepage website
+// Handler for main homepage website
 fun homePageHandler(request: Request): Response = Response(Status.OK, "This is Imperial.")
 
-//Handler for Department of Computing website
+// Handler for Department of Computing website
 fun compPageHandler(request: Request): Response = Response(Status.OK, "This is DoC.")
 
-//Handler for say-hello page
-//When called will return a greeting based on the url parameters
-//Uses a for loop to loop through all the parameters of the url and
-//adjusts the response body accordingly
+// Handler for say-hello page
+// When called will return a greeting based on the url parameters
+// Uses a for loop to loop through all the parameters of the url and
+// adjusts the response body accordingly
 fun helloHandler(request: Request): Response {
     var responseBody: String = "Hello, World!"
     val params = queryParams(request.url)
     for ((paramKey, paramValue) in params) {
         if (paramKey == "name") {
             responseBody = "Hello, $paramValue!"
-
         }
         if (paramKey == "style") {
             if (paramValue == "shouting") {
                 responseBody = responseBody.toUpperCase()
             }
         }
-
     }
     return Response(Status.OK, responseBody)
 }
