9f054d9c8759172174c4ad25d95565186ededb87
yashle1611 <yashpatel1611@gmail.com>
2020-11-20 11:08:54 +0000

    Cleaned up code

--- src/main/kotlin/webserver/WebServer.kt
+++ src/main/kotlin/webserver/WebServer.kt
@@ -1,24 +1,25 @@
 package webserver
 
-fun main() {
-    var urls = listOf("http://www.imperial.ac.uk/?q=xxx", "http://www.imperial.ac.uk/?q=xxx&rr=zzz", "http://www.imperial.ac.uk/")
-    for (i in urls) {
-        println(queryParams(i))
-    }
-}
-
 // write your web framework code here:
 
+//Uses the string function 'substringBefore' to get everything before
+//the first ':' character
 fun scheme(url: String): String = url.substringBefore(':')
 
+//Uses the spring split function to separate all the parts between '/'s
+//The second index of the set of separations is the host url
 fun host(url: String): String = url.split('/')[2]
 
+//Uses the substringAfter function on the host url to get the substring before the parameters
 fun path(url: String): String = url.substringAfter(host(url)).substringBefore("?")
 
+//Function to return the list of pair parameters in a url
+//The function first gets all the parameters and splits them into each pair
+//The pairs are looped through using a for loop and are added to the mutable list
 fun queryParams(url: String): List<Pair<String, String>> {
 
     val params = url.substringAfter("?").split('&')
-    var pairList = mutableListOf<Pair<String, String>>()
+    val pairList = mutableListOf<Pair<String, String>>()
 
     if (params[0] != url) {
         for (param in params) {
@@ -29,6 +30,32 @@ fun queryParams(url: String): List<Pair<String, String>> {
     return pairList
 }
 
+//List of pair values with the path and its corresponding handler
+var mappings = listOf(Pair("/say-hello", ::helloHandler), Pair("/", ::homePageHandler),
+        Pair("/computing", ::compPageHandler))
+
+fun route(request: Request): Response {
+    val pagePath = path(request.url)
+    for ((page, func) in mappings) {
+        if (page == pagePath) {
+            return func(request)
+        }
+    }
+    return Response(Status.NOT_FOUND, "")
+}
+
+// http handlers for a particular website...
+
+//Handler for main homepage website
+fun homePageHandler(request: Request): Response = Response(Status.OK, "This is Imperial.")
+
+//Handler for Department of Computing website
+fun compPageHandler(request: Request): Response = Response(Status.OK, "This is DoC.")
+
+//Handler for say-hello page
+//When called will return a greeting based on the url parameters
+//Uses a for loop to loop through all the parameters of the url and
+//adjusts the response body accordingly
 fun helloHandler(request: Request): Response {
     var responseBody: String = "Hello, World!"
     val params = queryParams(request.url)
@@ -46,26 +73,3 @@ fun helloHandler(request: Request): Response {
     }
     return Response(Status.OK, responseBody)
 }
-
-fun rootHandler(request: Request): Response {
-    return Response(Status.OK, "This is Imperial")
-}
-
-fun compHandler(request: Request): Response{
-    return Response(Status.OK, "This is DoC")
-}
-
-var mappings = listOf(Pair("/say-hello", ::helloHandler), Pair("/", ::rootHandler), Pair("/computing", ::compHandler))
-fun route(request: Request): Response {
-    var pagePath = path(request.url)
-    for((page, funct) in mappings){
-        if(page == pagePath){
-            funct
-        }
-    }
-    return Response(Status.NOT_FOUND, "")
-}
-
-// http handlers for a particular website...
-
-fun homePageHandler(request: Request): Response = Response(Status.OK, "This is Imperial.")
--- src/test/kotlin/webserver/WebServerTest.kt
+++ src/test/kotlin/webserver/WebServerTest.kt
@@ -60,22 +60,22 @@ class WebServerTest {
 
 // ***** Tests for Routing *****
 
-//  @Test
-//  fun `can route to hello handler`() {
-//    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred")
-//    assertEquals("Hello, Fred!", route(request).body)
-//  }
-//
-//  @Test
-//  fun `can route to homepage handler`() {
-//    assertEquals("This is Imperial.", route(Request("http://www.imperial.ac.uk/")).body)
-//    assertEquals("This is DoC.", route(Request("http://www.imperial.ac.uk/computing")).body)
-//  }
-//
-//  @Test
-//  fun `gives 404 when no matching route`() {
-//    assertEquals(Status.NOT_FOUND, route(Request("http://www.imperial.ac.uk/not-here")).status)
-//  }
+  @Test
+  fun `can route to hello handler`() {
+    val request = Request("http://www.imperial.ac.uk/say-hello?name=Fred")
+    assertEquals("Hello, Fred!", route(request).body)
+  }
+
+  @Test
+  fun `can route to homepage handler`() {
+    assertEquals("This is Imperial.", route(Request("http://www.imperial.ac.uk/")).body)
+    assertEquals("This is DoC.", route(Request("http://www.imperial.ac.uk/computing")).body)
+  }
+
+  @Test
+  fun `gives 404 when no matching route`() {
+    assertEquals(Status.NOT_FOUND, route(Request("http://www.imperial.ac.uk/not-here")).status)
+  }
 
 //  ***** Tests for the Extensions *****
 
