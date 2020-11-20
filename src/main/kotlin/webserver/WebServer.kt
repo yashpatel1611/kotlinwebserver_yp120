c81cc18f39c965a7a93851ef27cbc333628dc38a
yashle1611 <yashpatel1611@gmail.com>
2020-11-20 00:47:42 +0000

    Commit to transfer carry on working on laptop

--- src/main/kotlin/webserver/WebServer.kt
+++ src/main/kotlin/webserver/WebServer.kt
@@ -1,14 +1,33 @@
 package webserver
 
+fun main() {
+    var urls = listOf("http://www.imperial.ac.uk/?q=xxx", "http://www.imperial.ac.uk/?q=xxx&rr=zzz", "http://www.imperial.ac.uk/")
+    for(i in urls){
+        queryParams(i)
+    }
+}
+
 // write your web framework code here:
 
-fun scheme(url: String): String = TODO()
+fun scheme(url: String): String = url.substringBefore(':')
 
-fun host(url: String): String = TODO()
+fun host(url: String): String = url.split('/')[2]
 
-fun path(url: String): String = TODO()
+fun path(url: String): String = url.substringAfter(host(url)).substringBefore("?")
 
-fun queryParams(url: String): List<Pair<String, String>> = TODO()
+fun queryParams(url: String): List<Pair<String, String>> {
+
+    val params = url.substringAfter("?").split('&')
+    var pairList = mutableListOf<Pair<String, String>>()
+
+    if (params[0] != url) {
+        for (param in params) {
+            val keyValues = param.split('=')
+            pairList.add(Pair(keyValues[0], keyValues[1]))
+        }
+    }
+    return pairList
+}
 
 // http handlers for a particular website...
 
